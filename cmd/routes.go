abe22efc07355984120b54f8652dd7e69d5884fc
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2025-08-16 12:02:26 +0600

    refactor: make separate func for products route

--- cmd/routes.go
+++ cmd/routes.go
@@ -7,12 +7,24 @@ import (
 	"github.com/ecommarce/middleware"
 )
 
+// initRoutes initializes the routes for the application
+// and applies the middleware to each route.
+// It sets up the HTTP handlers for various endpoints.
 func initRoutes(mux *http.ServeMux, manager *middleware.Manager) {
 	mux.Handle("GET /ping", manager.With(http.HandlerFunc(basePathHandler)))
-	// products handlers
+	productRoutes(mux, manager)
+}
+
+// Products routes
+func productRoutes(mux *http.ServeMux, manager *middleware.Manager) {
+	// Get all products
 	mux.Handle("GET /products", manager.With(http.HandlerFunc(handlers.GetProducts)))
+	// Get product by product id
 	mux.Handle("GET /products/{id}", manager.With(http.HandlerFunc(handlers.GetProductById)))
-	mux.Handle("POST /products", manager.With(http.HandlerFunc(handlers.CreteProduct), middleware.Logger))
+	// Create a new product
+	mux.Handle("POST /products", manager.With(http.HandlerFunc(handlers.CreteProduct)))
+	// Update a product
 	mux.Handle("PUT /products/{id}", manager.With(http.HandlerFunc(handlers.UpdateProduct)))
+	// Delete a product
 	mux.Handle("DELETE /products/{id}", manager.With(http.HandlerFunc(handlers.DeleteProduct)))
 }
